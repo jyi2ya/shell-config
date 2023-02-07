8d211b45aebc7e6d11ea99fb8cddfe631b1a2a7f
jyi2ya <jyi2ya@outlook.com>
2023-02-07 15:42:43 +0800

    feat: run `tmux` inside ssh sessions

--- head.sh
+++ head.sh
@@ -5,3 +5,6 @@ case $- in
       *) return;;
 esac
 
+if command -v tmux >/dev/null && [ -z "$TMUX" ] && [ -n "$SSH_TTY" ]; then
+    exec sh -c 'tmux attach-session -t X || tmux new-session -s X'
+fi
