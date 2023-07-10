9a0c3ab7c7f76a2b11f288d85d47ec2bb74dff44
jyi2ya <jyi2ya@outlook.com>
2023-07-10 17:03:46 +0800

    fix: disable autostart for tmux

--- head.sh
+++ head.sh
@@ -5,6 +5,8 @@ case $- in
       *) return;;
 esac
 
-if command -v tmux >/dev/null && [ -z "$TMUX" ] && [ -n "$SSH_TTY" ]; then
-    exec sh -c 'tmux attach-session -t X || tmux new-session -s X'
-fi
+jf() {
+    tmux attach-session -t X || tmux new-session -s X
+}
+
+alias fj=jf
